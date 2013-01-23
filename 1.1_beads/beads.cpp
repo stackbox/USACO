b31696af5d19fd74affd4c494b1d12d0fedd9d5e
SageLui <superalsrk@foxmail.com>
2013-01-24 00:21:30 +0800

    1.1 beads

---
+++ 1.1_beads/beads.cpp
@@ -0,0 +1,39 @@
+/*
+ID: xi-lin2
+LANG: C++
+PROG: beads
+*/
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int countBeads(string necklace,int pos)
+{
+	chat tag;
+	int leftCount,rightCount,i,j,k;
+	
+}
+int main()
+{
+	ifstream fin("beads.in");
+	ofstream fout("beads.out");
+	
+	int N,i,j,k;
+	string str1,str2;
+	
+	fin >> N;
+	fin >> str1;
+	str2 = str1;
+	reverse(str2.begin(),str2.end());
+	
+	
+	
+	
+	return 0;
+}
