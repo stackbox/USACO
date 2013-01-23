0f3473ce6e7d19633be4e683334698e3c6c3b4c5
SageLui <superalsrk@foxmail.com>
2013-01-24 06:41:02 +0800

    modified:   1.1_beads/beads.cpp 	modified:   1.1_beads/beads.in 	modified:   1.1_beads/beads.out

--- 1.1_beads/beads.cpp
+++ 1.1_beads/beads.cpp
@@ -4,8 +4,6 @@ LANG: C++
 PROG: beads
 */
 
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -13,10 +11,36 @@ PROG: beads
 
 using namespace std;
 
-int countBeads(string necklace,int pos)
+
+string  getNewStr(string &s,const int pos)
 {
-	chat tag;
-	int leftCount,rightCount,i,j,k;
+	string tmp1 =  s.substr(pos) + s.substr(0,pos);
+	return  (tmp1 + tmp1);
+}
+
+int countBeads(string &s, int N)
+{
+	int leftCount=0,rightCount=0,i,j,k;
+	char tag = 'w';
+	int prePos = N;
+	while(1)
+	{
+		if(s[prePos] != tag && tag != 'w' && s[prePos] != 'w' || prePos == 2*N) break;
+		else if(s[prePos] == 'w') prePos++,rightCount++;
+		else tag = s[prePos],prePos++,rightCount++;
+	}
+	
+	prePos = N-1;
+	tag = 'w';
+	while(1)
+	{
+		if(s[prePos] != tag && tag != 'w' && s[prePos] != 'w' || prePos < 0 ) break;
+		else if(s[prePos] == 'w') prePos--,leftCount++;
+		else tag = s[prePos],prePos--,leftCount++;
+	}
+	
+	if(rightCount + leftCount > N) return N;
+	else return (rightCount + leftCount);
 	
 }
 int main()
@@ -24,16 +48,17 @@ int main()
 	ifstream fin("beads.in");
 	ofstream fout("beads.out");
 	
-	int N,i,j,k;
-	string str1,str2;
+	string str;
+	int N,maxVal = -1;
+	fin >> N >> str;
+	for(int i = 0; i < N; i++)
+	{
+		string temp = getNewStr(str,i);
+		int tempCount = countBeads(temp,N);
+		if(tempCount > maxVal) maxVal = tempCount;
+	}
 	
-	fin >> N;
-	fin >> str1;
-	str2 = str1;
-	reverse(str2.begin(),str2.end());
-	
-	
-	
+	fout << maxVal << endl;
 	
 	return 0;
 }
